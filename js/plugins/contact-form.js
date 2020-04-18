fe57fbf66674342ea5ef7702735d34d8355c9e86
shahriarfaisal <57552894+shahriarfaisal@users.noreply.github.com>
2020-04-18 22:16:54 +0600

    first commit

---
+++ js/plugins/contact-form.js
@@ -0,0 +1,73 @@
+
+(function ($, window, document, undefined) {
+  'use strict';
+
+  var form = $('#contact-form'),
+    messageContainer = $('#message-contact'),
+    messageText = $('#message-contact .message-text');
+
+
+  form.submit(function (e) {
+
+
+    // remove the error class
+    form.find('.form-group').removeClass('error');
+    messageContainer.removeClass('error');
+
+    var errorAll = '';
+
+    // get the form data
+    var formData = {
+      'name': $('input[name="form-name"]').val(),
+      'email': $('input[name="form-email"]').val(),
+      'message': $('textarea[name="form-message"]').val(),
+      'textfield': $('input[name="text-field"]').val()
+    };
+
+    // process the form
+    $.ajax({
+      type: 'POST',
+      url: 'php/contact-process.php',
+      data: formData,
+      dataType: 'json',
+      encode: true
+    }).done(function (data) {
+      // handle errors
+      if (!data.success) {
+
+        if (data.errors.name) {
+          $('#name-field').addClass('error');
+          errorAll = data.errors.name;
+        }
+
+        if (data.errors.email) {
+          $('#email-field').addClass('error');
+          errorAll = errorAll + ' ' + data.errors.email;
+        }
+
+        if (data.errors.message) {
+          $('#message-field').addClass('error');
+          errorAll = errorAll + ' ' + data.errors.message;
+        }
+        messageContainer.addClass('error');
+        messageText.html(errorAll);
+      } else {
+        // display success message
+        messageText.html(data.confirmation);
+
+        $('#contact-form .form-control').each(function () {
+          $(this).fadeIn().val($(this).attr('placeholder'));
+        });
+      }
+      messageContainer.slideDown('slow', 'swing');
+      setTimeout(function () {
+        messageContainer.slideUp('slow', 'swing');
+      }, 5000);
+    }).fail(function (data) {
+      // for debug
+      console.log(data)
+    });
+
+    e.preventDefault();
+  });
+}(jQuery, window, document));
